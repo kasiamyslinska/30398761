07b9fa49243c2e9f19ba9264cff9bb33eaae7cba
kasiamyslinska <83585480+kasiamyslinska@users.noreply.github.com>
2025-04-18 19:51:40 +0200

    Delete Program_3.py

--- Program_3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-get_ipython().run_cell_magic('file', 'app.py', "\nfrom flask import Flask, jsonify\n\napp = Flask(__name__)\n\n@app.route('/')\ndef home():\n    return jsonify('My second API')\n")
-
